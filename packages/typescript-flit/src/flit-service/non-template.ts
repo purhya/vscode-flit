6c8b5c353716f95447d8f8c7b3c88d9beb202fff
FF <pucelle@163.com>
2021-02-21 18:38:05 +0800

    Supports autocompletion for `flit.on(...)`.

--- packages/typescript-flit/src/flit-service/non-template.ts
+++ packages/typescript-flit/src/flit-service/non-template.ts
@@ -14,28 +14,42 @@ export function getSimulateTokenFromNonTemplate(fileName: string, offset: number
 		return null
 	}
 
-	// on(..., '|', ...)
-	if (typescript.isStringLiteral(node)
+	// ...on(..., '|')
+	if (!(typescript.isStringLiteral(node)
 		&& typescript.isCallExpression(node.parent)
-		&& typescript.isIdentifier(node.parent.expression)
-		&& (node.parent.expression.escapedText === 'on' || node.parent.expression.escapedText === 'off')
-		&& node.parent.arguments[1] === node
-	) {
-		let simulateToken: FlitToken = {
-			type: FlitTokenType.DomEvent,
-			closestCustomTagName: null,
-			tagName: '',
-			attrPrefix: '',
-			attrName: node.getText().replace(/['"]/g, ''),
-			attrValue: null,
-			nextTokenString: '',
-			start: node.getStart() + 1,
-			end: node.getEnd() - 1,
-			cursorOffset: offset - (node.getStart() + 1),
-		}
-
-		return {node, token: simulateToken}
+		&& node.parent.arguments[1] === node))
+	{
+		return null
 	}
 
-	return null
+	let exp = node.parent.expression
+
+	// on(...)
+	let beOnOff = typescript.isIdentifier(exp)
+		&& (exp.escapedText === 'on' || exp.escapedText === 'off')
+
+	// flit.on(...)
+	let beFlitOnOff = typescript.isPropertyAccessExpression(exp)
+		&& (exp.name!.escapedText === 'on' || exp.name!.escapedText === 'off')
+		&& exp.expression
+		&& typescript.isIdentifier(exp.expression) && exp.expression.text === 'flit'
+
+	if (!beOnOff && !beFlitOnOff) {
+		return null
+	}
+
+	let simulateToken: FlitToken = {
+		type: FlitTokenType.DomEvent,
+		closestCustomTagName: null,
+		tagName: '',
+		attrPrefix: '',
+		attrName: node.getText().replace(/['"]/g, ''),
+		attrValue: null,
+		nextTokenString: '',
+		start: node.getStart() + 1,
+		end: node.getEnd() - 1,
+		cursorOffset: offset - (node.getStart() + 1),
+	}
+
+	return {node, token: simulateToken}
 }
